c64caccafe268c1474529fcf698c7af33a348d59
hqrafei <humayoonrafei1@gmail.com>
2022-10-27 19:48:43 -0700

    THe classes are created

--- src/GameApp.java
+++ src/GameApp.java
@@ -3,20 +3,38 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
 
+
+import static javafx.scene.paint.Color.MAGENTA;
+import static javafx.scene.paint.Color.WHITE;
+
+
+//Class names always start with an upper-case letter
+//Variable names always start with a lower-case letter
+//Non-Constant identifiers use camel case
+//Constant identifiers use upper snake case
+//All code is neat and properly indented
+//You are restricted to an 80-character width
 public class GameApp extends Application {
     // The GameApp class sets up all keyboard event handlers
     // to invoke public methods in Game.
     private static final int APP_WIDTH = 400;
-    private static final int APP_HEIGHT = 800;
+    private static final int APP_HEIGHT = 600;
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group gRoot = new Group();
+        //Group gRoot = new Group();
+        Pane root = new Pane();
+        Scene scene = new Scene(root,APP_WIDTH,APP_HEIGHT);
 
-        Scene scene = new Scene(gRoot,APP_WIDTH,APP_HEIGHT);
+
+
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("GameAPP");
@@ -32,9 +50,56 @@ public class GameApp extends Application {
 
 class Game extends Pane {
     //all game logic and object construction belong here
+    // All of the Game rules are implemented in Game Class
+    //state of the game and determines win/lose
 
 
 }
 
+class GameObject extends Group{
+    //contains methods and fields that manage the
+    // common aspects of all game objects .
 
+}
 
+//class Body extends GameObject{
+//    public Body(){
+//        super();
+//        Ellipse body = new Ellipse();
+//        body.setRadiusx(10);
+//        body.setRadiusY(10);
+//        body.setFill(MAGENTA);
+//        add(Body);
+//    }
+//}
+//class FireOval extends GameObject {
+//    public FireOval (){
+//        Body myBody =//
+//
+//        add(m)
+//    }
+//}
+class cloud extends GameApp{
+    // represents a cloud in the sky
+
+    Circle cloudCircle = new Circle(6,WHITE);// Todo Add it as childern to the root
+    //groot.addchildern(cloudcircle)
+
+}
+
+class Helipad {
+    // Represents the starting and ending location for Helicopter
+    Rectangle rectangle = new Rectangle(23,34);
+   // rectangle.setStroke(COLOR.GRAY);
+
+}
+
+class Helicopter {
+    // most complex game object
+    // a yellow circle with a line to the direction of helicopter
+    // display the current fuel
+}
+
+class PondAndCloud {
+
+}
