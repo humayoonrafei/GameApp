58a8097739e5ff907360501c14bdec355a5689bd
hqrafei <humayoonrafei1@gmail.com>
2022-12-18 17:53:24 -0800

    Improvment : 1- The Helicopter is going forword 2- Any Angle which the helicopter is pointing to 3- Reverses back on the same angle 4- Every thing moves correctly 5- Every thing moves good now Challangle:---------
    1- To change the background
    it doesn't shwo up

--- src/GameApp.java
+++ src/GameApp.java
@@ -1,21 +1,17 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.*;
 import javafx.scene.shape.*;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.event.EventHandler;
 
 
 import java.util.Random;
@@ -51,10 +47,16 @@ public class GameApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //Group gRoot = new Group();
-        //Pane root = new Pane();
+        Pane root = new Pane();
+
         Ng = new Game();
-        Scene root = new Scene(Ng, 400, 600);
-        root.setFill(BLACK);
+        Scene scene = new Scene(Ng, 400, 600);
+
+
+
+
+       // Create a background image
+
 
 
         // key Listeners
@@ -63,32 +65,60 @@ public class GameApp extends Application {
 
         //root.getChildren().add(Ng);
 
-        primaryStage.setScene(root);
+        primaryStage.setScene(scene);
+
+        Image image = new Image("file:/Desert.png");
+        ImageView background = new ImageView(image);
+        background.setImage(image);
+        root.getChildren().add(background);
+
+
+
         primaryStage.setTitle("GameAPP");
-        primaryStage.show();
 
-        root.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+
+        AnimationTimer timer = new AnimationTimer() {
+
+
             @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case LEFT:
-                        Ng.HelicopterLeftTurn();
-                        break;
-                    case DOWN:
-                        Ng.HelicopterMoveDown();
-                        break;
-                    case UP:
-                        Ng.Move();
-                        break;
-                    case RIGHT:
-                        Ng.HelicopterRightTurn();
-                        break;
-                }
-            }
-        });
+            public void handle(long now) {
 
-    }
-}
+                root.setOnKeyPressed(e -> {
+
+                    switch (e.getCode()) {
+                        case I:
+                            //if (inside Helipad ) {
+
+                            Ng.Fuel(new Helipad());
+
+
+                            break;
+                        case LEFT:
+                            Ng.HelicopterLeftTurn();
+                            break;
+                        case DOWN:
+                            Ng.HelicopterMoveDown();
+                            break;
+                        case UP:
+                            Ng.Move();
+                            break;
+                        case RIGHT:
+                            Ng.HelicopterRightTurn();
+                            break;
+                    }
+
+
+                });
+            }
+            };
+
+                primaryStage.show();
+
+                timer.start();
+
+            }
+        }
 //Class Game
 //
 //For this first version all game logic and object construction belong in the Game class.
@@ -110,11 +140,18 @@ class Game extends Pane {
     final static double APP_WIDTH = 400;
 
     final static double APP_HEIGHT = 600;
+  //  Image image = new Image("Desert.png");
+  //  ImageView background = new ImageView(image);
 
     int HELICOPTER_FUEL = 25000;
     Helicopter helicopter;
+    int speed = 4;
+    int degreeLeft = 15;
+    int degreesRight = -15;
+
 
     public Game() {
+
         setScaleY(-1);
         // setting Helipad on the screen
         Helipad helipad = new Helipad();
@@ -136,18 +173,15 @@ class Game extends Pane {
         this.getChildren().add(pond);
         this.getChildren().add(cloud);
 
-        helicopter = new Helicopter(helipad.myTranslation.getX(), helipad.myTranslation.getY());
+        helicopter = new Helicopter(helipad.myTranslation.getX(), helipad.myTranslation.getY(), "F: " + HELICOPTER_FUEL);
 
         this.getChildren().add(helicopter);
 
 
-        // Function for turning the Helicopter to the left
-
+        Text text = new Text("F:" + HELICOPTER_FUEL);
 
     }
 
-    int speed = 4;
-    int degreeLeft = 15;
 
     public void HelicopterLeftTurn() {
 
@@ -162,8 +196,6 @@ class Game extends Pane {
         //     helicopter.translate(Math.cos(Math.toRadians(helicopter.myTranslation.getX())), Math.cos(Math.toRadians(helicopter.myTranslation.getY())) );
         //   ++degreeLeft;
     }
-
-    int degreesRight = -15;
 
     public void HelicopterRightTurn() {
 
@@ -181,6 +213,28 @@ class Game extends Pane {
 //        --degrees;
 //        System.out.println("degrees"+degrees);
     }
+
+
+    int FUEL_DECREMENT = 2;
+    int UPDATE_INTERVAL = 1;
+    public void Fuel(Helipad helipad) {
+
+
+        if (helicopter.intersects(helipad.getBoundsInParent())) {
+            HELICOPTER_FUEL = 25000;
+        } else {
+
+            HELICOPTER_FUEL = HELICOPTER_FUEL - FUEL_DECREMENT;
+
+            System.out.print("After : " + HELICOPTER_FUEL);
+        }
+    }
+
+
+
+
+
+
 
 
     public void Move() {
@@ -206,10 +260,11 @@ class Game extends Pane {
 
 ///
 
+}
 
-    interface Updatable {
-        void update();
-    }
+interface Updatable {
+    void update();
+}
 
 
 //Game Object Classes
@@ -227,57 +282,56 @@ class Game extends Pane {
 // Any state or behavior in this class should apply to all game object this. For example, the helicopter can move, while a pond cannot.
 // Consequently, you would not include anything regarding movement in this class.
 
-    class GameObject extends Group implements Updatable {
-        //contains methods and fields that manage the
-        // common aspects of all game objects .
+class GameObject extends Group implements Updatable {
+    //contains methods and fields that manage the
+    // common aspects of all game objects .
 
 
-        // The GameApp class sets up all keyboard event handlers
-        // to invoke public methods in Game.
+    // The GameApp class sets up all keyboard event handlers
+    // to invoke public methods in Game.
 
 
-        protected Translate myTranslation;
-        protected Rotate myRotation;
-        protected Scale myScale;
+    protected Translate myTranslation;
+    protected Rotate myRotation;
+    protected Scale myScale;
 
-        public GameObject() {
-            myTranslation = new Translate();
-            myRotation = new Rotate();
-            myScale = new Scale();
-            this.getTransforms().addAll(myTranslation, myRotation, myScale);
-        }
+    public GameObject() {
+        myTranslation = new Translate();
+        myRotation = new Rotate();
+        myScale = new Scale();
+        this.getTransforms().addAll(myTranslation, myRotation, myScale);
+    }
 
-        public void rotate(double degrees) {
-            myRotation.setAngle(degrees);
-            myRotation.setPivotX(0);
-            myRotation.setPivotY(0);
-        }
+    public void rotate(double degrees) {
+        myRotation.setAngle(degrees);
+        myRotation.setPivotX(0);
+        myRotation.setPivotY(0);
+    }
 
-        public void scale(double sx, double sy) {
-            myScale.setX(sx);
-            myScale.setY(sy);
-        }
+    public void scale(double sx, double sy) {
+        myScale.setX(sx);
+        myScale.setY(sy);
+    }
 
-        public void translate(double tx, double ty) {
-            myTranslation.setX(tx);
-            myTranslation.setY(ty);
-        }
+    public void translate(double tx, double ty) {
+        myTranslation.setX(tx);
+        myTranslation.setY(ty);
+    }
 
-        public double getMyRotation() {
-            return myRotation.getAngle();
-        }
+    public double getMyRotation() {
+        return myRotation.getAngle();
+    }
 
-        public void update() {
-            for (Node n : getChildren()) {
-                if (n instanceof Updatable)
-                    ((Updatable) n).update();
-            }
-        }
-
-        void add(Node node) {
-            this.getChildren().add(node);
+    public void update() {
+        for (Node n : getChildren()) {
+            if (n instanceof Updatable) ((Updatable) n).update();
         }
     }
+
+    void add(Node node) {
+        this.getChildren().add(node);
+    }
+}
 
 
 //Class Pond
@@ -285,140 +339,137 @@ class Game extends Pane {
 //This class represents a pond or lake in the Central Valley.
 // For this first version of the project,
 // we will abstract the pond as a simple blue circle placed at random such that it does not intersect any other ground based object.
+class Pond extends GameObject {
 
-    class Pond extends GameObject {
-
-        public Pond() {
-            Circle pondCircle = new Circle(10);
-            int pondRadius = 10;
-            pondCircle.setRadius(pondRadius);// Todo Add it as childern to the root
-            pondCircle.setFill(BLUE);
-            this.getChildren().add(pondCircle);
-        }
-
-
+    public Pond() {
+        Circle pondCircle = new Circle(10);
+        int pondRadius = 10;
+        pondCircle.setRadius(pondRadius);// Todo Add it as childern to the root
+        pondCircle.setFill(BLUE);
+        this.getChildren().add(pondCircle);
     }
 
 
-    class Cloud extends GameObject {
-        // represents a cloud in the sky
-        public Cloud() {
-            Circle cloudCircle = new Circle(50);
-            int cloudRadius = 50;
-            cloudCircle.setRadius(cloudRadius);// Todo Add it as childern to the root
-            this.getChildren().add(cloudCircle);
-        }
+}
+
+class Cloud extends GameObject {
+    // represents a cloud in the sky
+    public Cloud() {
+        Circle cloudCircle = new Circle(50);
+        int cloudRadius = 50;
+        cloudCircle.setRadius(cloudRadius);// Todo Add it as childern to the root
+        this.getChildren().add(cloudCircle);
     }
+}
 
-    class Helipad extends GameObject {
-        // Represents the starting and ending location
-
-
-        double Helipad_Height = 80;
-        double Helipad_Width = 80;
-
-        double Helipad_Position = 100;
-
-        public Helipad() {
+class Helipad extends GameObject {
+    // Represents the starting and ending location
 
 
-            Rectangle Helipad = new Rectangle(Helipad_Width, Helipad_Height);
+    double Helipad_Height = 80;
+    double Helipad_Width = 80;
 
-            Helipad.setStroke(GRAY);
+    double Helipad_Position = 100;
 
-
-            double Circle_Radius = 30;
-            Circle circle_Helipad = new Circle((Circle_Radius));
-
-            //circle_Helipad.setTranslateY(Helipad_Height/2);
-            Helipad.setX((circle_Helipad.getCenterX() - Helipad.getWidth()) / 2);
-            Helipad.setY((circle_Helipad.getCenterY() - Helipad.getHeight()) / 2);
-            //circle_Helipad.setTranslateX((Helipad_Width)/2);
-            //circle_Helipad.setRadius();
-            circle_Helipad.setStroke(GRAY);
-            translate(Game.APP_WIDTH / 2, Helipad_Height);
+    public Helipad() {
 
 
-            //circle_Helipad.setTranslateX(Helipad.getTranslateX()+circle_Helipad.getRadius()+2);
-            //circle_Helipad.setTranslateY(Helipad.getTranslateY()+circle_Helipad.getRadius());
+        Rectangle Helipad = new Rectangle(Helipad_Width, Helipad_Height);
 
-            this.getChildren().add(Helipad);
-            this.getChildren().add(circle_Helipad);
-
-        }
-    }
-
-    class Helicopter extends GameObject {
-
-        // most complex game object
-        // a yellow circle with a line to the direction of helicopter
-        // display the current fuel
-        // public void rotateLeft() is when you do LEFT Arrow key
-        //{ rotate(-15); }
+        Helipad.setStroke(GRAY);
 
 
-        double Helicopeter_Radius = 10;
+        double Circle_Radius = 30;
+        Circle circle_Helipad = new Circle((Circle_Radius));
 
-        public Helicopter(double x, double y) {
-
-
-            Circle Helicoptercircle = new Circle(Helicopeter_Radius);
-
-            Helicoptercircle.setFill(YELLOW);
-
-            //Image helicopterImage = new Image("/Users/hqrafei/Documents/projects/GameApp/Pictures/Helicopter.png");
-
-            //ImageView iv1 = new ImageView();
-            //iv1.setImage(helicopterImage);
+        //circle_Helipad.setTranslateY(Helipad_Height/2);
+        Helipad.setX((circle_Helipad.getCenterX() - Helipad.getWidth()) / 2);
+        Helipad.setY((circle_Helipad.getCenterY() - Helipad.getHeight()) / 2);
+        //circle_Helipad.setTranslateX((Helipad_Width)/2);
+        //circle_Helipad.setRadius();
+        circle_Helipad.setStroke(GRAY);
+        translate(Game.APP_WIDTH / 2, Helipad_Height);
 
 
-            this.getChildren().add(Helicoptercircle);
+        //circle_Helipad.setTranslateX(Helipad.getTranslateX()+circle_Helipad.getRadius()+2);
+        //circle_Helipad.setTranslateY(Helipad.getTranslateY()+circle_Helipad.getRadius());
 
-            // Helicoptercircle.setTranslateX(Game.APP_WIDTH/2);
-            //Helicoptercircle.setTranslateY((Game.APP_HEIGHT-Helicopeter_Radius)/4.59);
-            translate(x, y); // moves Helicopter object by x and y
-            Line line = new Line();
-            line.setStartX(Helicoptercircle.getTranslateX());
-            line.setStartY(Helicoptercircle.getTranslateY());
-            line.setEndX(Helicoptercircle.getTranslateX());
-            line.setEndY((Helicoptercircle.getTranslateY() +16));
-            line.setStroke(YELLOW);
-            line.setStrokeWidth(3);
-
-
-            this.getChildren().add(line);
-
-
-
-
-            System.out.println(x + " " + y);
-
-
-            Text text = new Text(" ABC DE ");
-            text.setStroke(YELLOW);
-
-            text.setScaleY(-1);
-
-            text.setTranslateY(-15);
-            text.setTranslateX(-30);
-
-            ;
-            //
-
-            this.getChildren().add(text);
-
-
-        }
-
-        public void rotateLeft() {
-            rotate(15);
-        }
-
-    }
-
-    class PondAndCloud {
+        this.getChildren().add(Helipad);
+        this.getChildren().add(circle_Helipad);
 
     }
 }
+
+class Helicopter extends GameObject {
+
+    // most complex game object
+    // a yellow circle with a line to the direction of helicopter
+    // display the current fuel
+    // public void rotateLeft() is when you do LEFT Arrow key
+    //{ rotate(-15); }
+
+
+    double Helicopeter_Radius = 10;
+
+    public Helicopter(double x, double y, String HELICOPTER_FUEL) {
+
+
+        Circle Helicoptercircle = new Circle(Helicopeter_Radius);
+
+        Helicoptercircle.setFill(YELLOW);
+
+        //Image helicopterImage = new Image("/Users/hqrafei/Documents/projects/GameApp/Pictures/Helicopter.png");
+
+        //ImageView iv1 = new ImageView();
+        //iv1.setImage(helicopterImage);
+
+
+        this.getChildren().add(Helicoptercircle);
+
+        // Helicoptercircle.setTranslateX(Game.APP_WIDTH/2);
+        //Helicoptercircle.setTranslateY((Game.APP_HEIGHT-Helicopeter_Radius)/4.59);
+        translate(x, y); // moves Helicopter object by x and y
+        Line line = new Line();
+        line.setStartX(Helicoptercircle.getTranslateX());
+        line.setStartY(Helicoptercircle.getTranslateY());
+        line.setEndX(Helicoptercircle.getTranslateX());
+        line.setEndY((Helicoptercircle.getTranslateY() + 16));
+        line.setStroke(YELLOW);
+        line.setStrokeWidth(3);
+
+
+        this.getChildren().add(line);
+
+
+        System.out.println(x + " " + y);
+
+
+
+        Text text = new Text(HELICOPTER_FUEL );
+        text.setStrokeWidth(0.4);
+        text.setStroke(YELLOW);
+
+        text.setScaleY(-1);
+
+        text.setTranslateY(-15);
+        text.setTranslateX(-30);
+
+        //
+
+        this.getChildren().add(text);
+
+
+    }
+
+    public void rotateLeft() {
+        rotate(15);
+    }
+
+}
+
+class PondAndCloud {
+
+}
+
 
 
